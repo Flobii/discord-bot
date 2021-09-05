30c97a5dea68428e306b8e3375cf653b4464e6b5
Florian Obernbrger <obernbef@dachsberg.at>
2021-09-05 13:33:10 +0200

    Add Card_FromFile and finish it

--- data/card.jl
+++ data/card.jl
@@ -1,23 +1,50 @@
 using TOML
-# using Images, TODO: ERROR HERE FLOBII, CUNT
+using Base: open
+
+include("event.jl")
+
+struct File
+    adress::String
+end
+
+struct Url
+    adress::String
+end
 
 struct Card
     name::String
-    # events::[]
+    events::Vector{Event}
     description::String
-    # image::String
+    image::Union{File,Url}
 end
 
 """
     Card_FromFile(file::AbstractString)
 
 # Arguments
-- `file::AbstractString`: The file  to 
+- `file::AbstractString`: The file storing the card(s)
+                          see `example.toml` for structure
 """
 function Card_FromFile(file::AbstractString)
     cards = Card[]
     file = TOML.parsefile(file)
     for name in keys(file)
-        push!(cards, Card(name, file[name]))
+        # manipulate image to later determine if file or url
+        image = lowercase(file[name]["image"])
+        if startswith(image, "url:")
+            image = Url(image)
+        elseif startswith(image, "file:")
+            image = File(replace(image))
+        else
+            @warn "Cannot determine image type of image for card $name"
+        end
+    
+        # create events for card
+        events = Event_CreateEvent.(file[name]["events"])
+
+        description = file[name]["description"]
+        push!(cards, Card(name, events, description, image))
     end
+
+    return cards
 end
